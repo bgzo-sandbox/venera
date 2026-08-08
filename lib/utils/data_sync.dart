931f71f19524eac5486fb8cc1fd9dd6bf3702d9a
HX(Only Apple Can Do) <57313137+bgzo@users.noreply.github.com>
2026-08-09 00:47:00 +0800

    chore(updates): remove obsolete 'once a day' hint

--- lib/utils/data_sync.dart
+++ lib/utils/data_sync.dart
@@ -396,7 +396,7 @@ class DataSync with ChangeNotifier {
             }
           }
         }
-
+        // TODO
         Log.info("Upload Data", "Data uploaded successfully");
         return const Res(true);
       } catch (e, s) {
